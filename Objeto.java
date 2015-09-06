22ef833a35b28848ed81a95bde5a635125e9d9d1
brayanrp215 <brayanrp215@gmail.com>
2015-09-06 00:55:24 -0500

    MasClases

--- Objeto.java
+++ Objeto.java
@@ -1,4 +1,5 @@
 
 public class Objeto {
-
+   // Clase principal de la cual van a a heredad todos los objetos del juego	
+	
 }
--- Personaje.java
+++ Personaje.java
@@ -1,4 +1,6 @@
 
 public class Personaje {
-
+    // Clase de la cual se van a heredad todos los personajes com A, B enemigos , mosutros aledaños 
+	
 }
+ 
